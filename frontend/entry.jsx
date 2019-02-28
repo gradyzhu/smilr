ef54211de021fd41b28dc809b1b3e149074dab19
your name here <gradyzhoo@gmail.com>
2019-02-27 18:01:51 -0800

    Should work push

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { login } from './actions/session_actions';
-import { signup } from './actions/session_actions';
-import { logout } from './actions/session_actions';
+// import { login } from './actions/session_actions';
+// import { signup } from './actions/session_actions';
+// import { logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
-  const root = document.getElementById('root');
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout; 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  ReactDOM.render(<Root store={store}/>, root);
+  // const store = configureStore();
+  // const root = document.getElementById('root');
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout; 
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
@@ -10,7 +10,7 @@ const configureStore = (preloadedState = {}) => (
     preloadedState,
     applyMiddleware(thunk, logger)
   )
-);
+)
 
 
 export default configureStore;
