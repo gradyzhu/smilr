432457c3febad187721bacf05ad86287de78ce1d
your name here <gradyzhoo@gmail.com>
2019-03-04 17:19:44 -0800

    Heroku push, update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
 ActiveRecord::Base.transaction do
   # user = User.create!({username: "", email: "", password: ""})
-  tony = User.create!(username: "tony", email: "tony@gmail.com", password: "password")
+  josh = User.create!(username: "josh", email: "josh@gmail.com", password: "password")
   juice = User.create!(username: "juice", email: "stephens@gmail.com", password: "password")
-  jason = User.create!(username: "jason", email: "jacegamer@gmail.com", password: "password")
 
   # photo = Photo.create!({user_id: , title: "", description: "", album_id: , date_taken: ""})
-  photo1 = Photo.create!(user_id: juice.id, title: "skate1", description: "fence")
-  photo2 = Photo.create!(user_id: juice.id, title: "skate2", description: "skating")
-  photo3 = Photo.create!(user_id: juice.id, title: "skate3", description: "odd angle")
-  photo4 = Photo.create!(user_id: juice.id, title: "skate4", description: "fuzzy")
-  photo5 = Photo.create!(user_id: juice.id, title: "skate5", description: "flying")
-  photo6 = Photo.create!(user_id: jason.id, title: "sf1", description: "bridge")
-  photo7 = Photo.create!(user_id: jason.id, title: "sf2", description: "gordos")
-  photo8 = Photo.create!(user_id: jason.id, title: "sf3", description: "lady")
+  josh_photo_1 = Photo.create!(user_id: josh.id, title: "josh_photo_1", description: "description")
+  josh_photo_2 = Photo.create!(user_id: josh.id, title: "josh_photo_2", description: "description")
+  josh_photo_3 = Photo.create!(user_id: josh.id, title: "josh_photo_3", description: "description")
+  josh_photo_4 = Photo.create!(user_id: josh.id, title: "josh_photo_4", description: "description")
+  josh_photo_5 = Photo.create!(user_id: josh.id, title: "josh_photo_5", description: "description")
+  josh_photo_6 = Photo.create!(user_id: josh.id, title: "josh_photo_6", description: "description")
+  josh_photo_7 = Photo.create!(user_id: josh.id, title: "josh_photo_7", description: "description")
+  josh_photo_8 = Photo.create!(user_id: josh.id, title: "josh_photo_8", description: "description")
 
+  juice_photo_1 = Photo.create!(user_id: juice.id, title: "juice_photo_1", description: "description")
+
+  # photoname.image.attach(io: open(""), filename: "")
   # attach url to photos
-  photo1.image.attach(io: File.open("/Users/grady/Desktop/assets/juice_photos/austin-ban-712-unsplash.jpg"), filename: "austin-ban-712-unsplash.jpg")
-  photo2.image.attach(io: File.open("/Users/grady/Desktop/assets/juice_photos/charlotte-butcher-345900-unsplash.jpg"), filename: "charlotte-butcher-345900-unsplash.jpg")
-  photo3.image.attach(io: File.open("/Users/grady/Desktop/assets/juice_photos/daan-stoof-1217441-unsplash.jpg"), filename: "daan-stoof-1217441-unsplash.jpg")
-  photo4.image.attach(io: File.open("/Users/grady/Desktop/assets/juice_photos/karol-kaczorek-753195-unsplash.jpg"), filename: "karol-kaczorek-753195-unsplash")
-  photo5.image.attach(io: File.open("/Users/grady/Desktop/assets/juice_photos/liene-geidane-61048-unsplash.jpg"), filename: "gordo")
-  photo6.image.attach(io: File.open("/Users/grady/Desktop/assets/jason_photos/sf/bridge.jpg"), filename: "bridge.jpg")
-  photo7.image.attach(io: File.open("/Users/grady/Desktop/assets/jason_photos/sf/lady.jpg"), filename: "lady.jpg")
-  photo8.image.attach(io: File.open("/Users/grady/Desktop/assets/jason_photos/sf/sf.jpg"), filename: "sf.jpg")
+  josh_photo_1.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1279580-unsplash.jpg"), filename: "josh-hild-1279580-unsplash.jpg")
+  josh_photo_2.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1289167-unsplash.jpg"), filename: "josh-hild-1289167-unsplash.jpg")
+  josh_photo_3.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1289181-unsplash.jpg"), filename: "josh-hild-1289181-unsplash.jpg")
+  josh_photo_4.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1313362-unsplash.jpg"), filename: "josh-hild-1313362-unsplash.jpg")
+  josh_photo_5.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1313363-unsplash.jpg"), filename: "josh-hild-1313363-unsplash.jpg")
+  josh_photo_6.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1330241-unsplash.jpg"), filename: "josh-hild-1330241-unsplash.jpg")
+  josh_photo_7.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1336479-unsplash.jpg"), filename: "josh-hild-1336479-unsplash.jpg")
+  josh_photo_8.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/josh_photos/josh-hild-1362788-unsplash.jpg"), filename: "josh-hild-1362788-unsplash.jpg")
+  
+  juice_photo_1.image.attach(io: open("https://s3-us-west-1.amazonaws.com/smilr-seed/juice_photos/amogh-manjunath-774717-unsplash.jpg"), filename: "amogh-manjunath-774717-unsplash.jpg")
 end
